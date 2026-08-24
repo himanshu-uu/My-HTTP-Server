9fe78d9cbc2d19d1c9d64977f38928d5fbd1fabb
Himanshu Shekhar <himanshu.25bce7420@vitapstudent.ac.in>
2026-08-24 12:12:03 +0000

    Constructed HTTP response

--- src/server.js
+++ src/server.js
@@ -6,6 +6,13 @@ const server = net.createServer( (socket) => {
     socket.on( "data", (data) => {
     console.log(data.toString())
     });
+    
+    socket.write(
+        "HTTP/1.1 200 OK\r\n" + 
+        "Content-Type: application/json\r\n\r\n"+ 
+        JSON.stringify( {
+            message: "This is my HTTP response"})
+    );
 });
 
 server.listen(3000);
