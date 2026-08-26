d8d4c81cfddee63b0d2cda2d5a3a163d9668e1c6
Himanshu Shekhar <himanshu.25bce7420@vitapstudent.ac.in>
2026-08-26 11:51:18 +0000

    Handling multiple requests in multiple chunks

--- src/server.js
+++ src/server.js
@@ -19,42 +19,22 @@ const server = net.createServer((socket) => {
             const headerEndIndex = buffer.indexOf("\r\n\r\n");
             const headerPart = buffer.slice(0, headerEndIndex);
 
+            const request = parseHeaders(headerPart);
 
-            const lines = headerPart.split("\r\n");
-            const requestLine = lines[0].split(" ");
-            const header = {};
             const bodyStartIndex = headerEndIndex + 4;       // body starts after the blank line \r\n\r\n
-
-            for (let i = 1; i < lines.length; i++) {
-
-                const colonIndex = lines[i].indexOf(":");
-                const key = lines[i].slice(0, colonIndex);
-                const value = lines[i].slice(colonIndex + 1).trim();
-                header[key] = value;
-            };
-
-            const request = {
-                method: requestLine[0],
-                path: requestLine[1],
-                version: requestLine[2],
-                headers: header,
-            };
 
             if (request.headers["Content-Length"]) {
 
                 const contentLength = Number(request.headers["Content-Length"]);
-                const availaibleBody = buffer.slice(bodyStartIndex);
-                if (Buffer.byteLength(availaibleBody) < contentLength) {
+                const availableBody = buffer.slice(bodyStartIndex);
+                if (Buffer.byteLength(availableBody) < contentLength) {
                     return;
                 }
 
                 const requestBody = buffer.slice(bodyStartIndex, bodyStartIndex + contentLength);
 
-                if (request.headers["Content-Type"] === "application/json") {
-                    request.body = JSON.parse(requestBody)
-                } else {
-                    request.body = requestBody;
-                }
+                request.body = parseBody(request.headers["Content-Type"], requestBody);
+
                 const requestEndIndex = bodyStartIndex + contentLength;
                 buffer = buffer.slice(requestEndIndex);     // removing the current request from buffer after it is parsed
             }
@@ -68,7 +48,7 @@ const server = net.createServer((socket) => {
             socket.write(
                 "HTTP/1.1 200 OK\r\n" +
                 "Content-Type: application/json\r\n" +
-                `Content-Length: ${contentLength}\r\n\r\n` +
+                `Content-Length: ${responseContentLength}\r\n\r\n` +
                 responseBody
             );
         }
@@ -79,6 +59,35 @@ const responseBody = JSON.stringify({
     message: "This is my HTTP response"
 });
 
-const contentLength = Buffer.byteLength(responseBody);
+const responseContentLength = Buffer.byteLength(responseBody);
+
+function parseHeaders(headerPart) {
+    const lines = headerPart.split("\r\n");
+    const requestLine = lines[0].split(" ");
+    const header = {};
+
+    for (let i = 1; i < lines.length; i++) {
+
+        const colonIndex = lines[i].indexOf(":");
+        const key = lines[i].slice(0, colonIndex);
+        const value = lines[i].slice(colonIndex + 1).trim();
+        header[key] = value;
+    };
+
+    const request = {
+        method: requestLine[0],
+        path: requestLine[1],
+        version: requestLine[2],
+        headers: header,
+    };
+    return request;
+}
+
+function parseBody(contentType, requestBody) {
+    if (contentType === "application/json") {
+        return JSON.parse(requestBody)
+    }
+    return requestBody;
+}
 
 server.listen(3000);
